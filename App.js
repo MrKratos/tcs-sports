575b367242da3e623db547b68415ca4c69feac85
Stalyn-1994 <stalyn.vaz@gmail.com>
2021-10-13 01:08:02 -0500

    primera pantalla
    campos jugadores y años

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { TextInput, StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 
 const Stack = createNativeStackNavigator();
+
+
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>TCS - SPORTS!</Text>
@@ -32,17 +35,51 @@ function TeamScreen() {
   );
 }
 
+
 function HomeScreen({ navigation }) {
+
+  const [number, onChangeNumber] = React.useState(null);
+  const [number_2, onChangeNumber_2] = React.useState(null);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Siguiente"
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+      <SafeAreaView>
+
+        <Text style={styles.text}>Número de jugadores</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber_2}
+          value={number_2}
+          placeholder="jugadores"
+          keyboardType="numeric"
+        />
+
+        <Text style={styles.text}>Número de años</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="años"
+          keyboardType="numeric"
+        />
+
+      </SafeAreaView>
+
+      <Button style={styles.division}
+        title="Jugar"
         onPress={() => navigation.navigate('Equipos')}
       />
+
+
+
     </View>
   );
 }
+
+
+
+
+
 //styles
 const styles = StyleSheet.create({
   container: {
@@ -50,9 +87,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
   },
+  division: {
+    alignItems: 'rigth',
+
+  },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
     marginTop: 50,
     paddingVertical: 10,
     paddingHorizontal: 32,
@@ -64,5 +105,15 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 18
   },
-
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  text: {
+    height: 30,
+    margin: 3,
+    padding: 10,
+  },
 });
