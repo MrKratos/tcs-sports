cbde2efd5e75c53ff8da1cb3ebd6dfd4859dbb9a
Stalyn-1994 <stalyn.vaz@gmail.com>
2021-10-19 11:27:09 -0500

    guardaDatos

--- App.js
+++ App.js
@@ -8,8 +8,6 @@ import { get, isEmpty, size } from 'lodash';
 import shortid from 'shortid';
 //import { Input,  } from 'react-native-elements';
 
-
-
 const Stack = createNativeStackNavigator();
 class App extends React.Component {
   constructor() {
@@ -18,16 +16,25 @@ class App extends React.Component {
       id: 'ss',
       task: 'aa',
       modalVisible: false,
+
     }
-    storeData('@key1', null)
+    //global variables
+    global.MyVar = [];
+    global.teamA = [];
+    global.teamB = [];
+    global.cont = 0;
+    global.players = null;
+    global.years = null;
+
   }
+
+
 
   render() {
 
     return (
       <View style={styles.container}>
         {/* <Text>TCS - SPORTS!</Text> */}
-
         <StatusBar style="auto" />
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Home" screenOptions={{ headerStyle: { backgroundColor: "#f22275" }, headerTitleStyle: { fontSize: 20 }, headerTitleAlign: "center", headerTintColor: "#fff" }}  >
@@ -44,38 +51,46 @@ class App extends React.Component {
       </View>
     );
   }
+
+
+
+
+
 }
 
 
 
 // Storage Function key-value
-const storeData = async (key, value) => {
-  try {
-    const jsonValue = JSON.stringify(value)
-    await AsyncStorage.setItem(key, jsonValue)
-  } catch (e) {
-    // save error
-  }
-}
+// const storeData = async (key, value) => {
+//   try {
+//     const jsonValue = JSON.stringify(value)
+//     await AsyncStorage.setItem(key, jsonValue)
+//   } catch (e) {
+// save error
+//   }
+// }
 // Getdata key
-const getData = async (value) => {
-  try {
-    const jsonValue = await AsyncStorage.getItem(value)
-    return jsonValue != null ? JSON.parse(jsonValue) : null
-  } catch (e) {
-    // read error
-  }
-}
+// const getData = async (value) => {
+//   try {
+//     const jsonValue = await AsyncStorage.getItem(value)
+//     return await AsyncStorage.getItem(jsonValue)
+//   } catch (e) {
+// read error
+//   }
+// }
+
 
 function EquipoScreen({ navigation, route }) {
   const [modalVisible, setModalVisible] = useState(false);
   const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(global.MyVar);
   const [editMode, setEditMode] = useState(false)
   const [id, setId] = useState("")
-
+  global.years = route.params.datos[0]
+  global.players = route.params.datos[1]
 
   const addTask = async (e) => {
+
     e.preventDefault()
     if (isEmpty(task)) {
       console.log("task empty")
@@ -87,29 +102,33 @@ function EquipoScreen({ navigation, route }) {
       setTask("")
       return
     }
-
-
+    global.cont = global.cont + 1
     const newTask = {
-      id: shortid.generate(),
+      id: global.cont,
       name: task,
       years: route.params.datos[0],
       players: route.params.datos[1],
-
     }
 
     setTasks([...tasks, newTask])
     console.log([...tasks, newTask])
     setTask("")
+
     // storeData('@key1', [...tasks, newTask])
+
     await AsyncStorage.setItem('@key1', JSON.stringify([...tasks, newTask]))
     const currentUser = await AsyncStorage.getItem('@key1')
     console.log('es' + currentUser)
+    global.MyVar = [...tasks, newTask];
+
   }
 
 
   const deleteTask = (id) => {
     const filteredTasks = tasks.filter(task =>
       task.id != id)
+    global.cont = global.cont -
+      +1
     setTasks(filteredTasks)
   }
 
@@ -120,7 +139,6 @@ function EquipoScreen({ navigation, route }) {
   }
 
   const saveTask = () => {
-
     //setTasks([...tasks, newTask])
     const editedTasks = tasks.map(item => item.id === id ? { id, name: task } : item)
     setTasks(editedTasks)
@@ -129,8 +147,10 @@ function EquipoScreen({ navigation, route }) {
     setId("")
   }
 
+  const navigateParams = () => {
 
 
+  }
   return (
     <View style={styles.container}>
 
@@ -192,7 +212,7 @@ function EquipoScreen({ navigation, route }) {
                           color='#47d170'
                           style={styles.buttonList}
                           title="Jugadores"
-                          onPress={() => navigation.navigate('Jugadores', { names: [task.id, task.name, task.years, task.players] })}
+                          onPress={() => navigation.navigate('Jugadores', { names: [task.id, task.name, global.years, global.players] })}
                         />
                         <Button
                           color='#479cd1'
@@ -225,13 +245,14 @@ function HomeScreen({ navigation }) {
   const [modalVisible, setModalVisible] = useState(false);
   const [number_2, onChangeNumber_2] = React.useState(null);
   const storageAndNavigation = () => {
-    console.log(number + " " + number_2)
     if (number === null || number === "" || number_2 === null || number_2 === "") {
+
       setModalVisible(true)
       return
     } else {
       // storeData('@key1', number)
       // storeData('@key2', number_2)
+      console.log(number + "  " + number_2)
       return navigation.navigate('Equipos', { datos: [number, number_2] })
     }
   }
@@ -299,11 +320,14 @@ function HomeScreen({ navigation }) {
 }
 
 function PlayerScreen({ navigation, route }) {
+
   const [modalVisible, setModalVisible] = useState(false);
   const [task, setTask] = useState("");
   const [total, setTotal] = useState(route.params.names[2]);
   const [age, setAge] = useState("");
-  const [tasks, setTasks] = useState([]);
+  //ingreso
+  const [tasks, setTasks] = useState();
+
   const [editMode, setEditMode] = useState(false)
   const [id, setId] = useState("")
   const [resultadoPlayer, setResult] = useState(route.params.names[3]);
@@ -353,8 +377,16 @@ function PlayerScreen({ navigation, route }) {
 
     setResult(route.params.names[3] - size([...tasks, newTask]));
     console.log([...tasks, newTask])
+    if (route.params.names[0] == 1) {
+      global.teamA = [...tasks, newTask]
+    }
+    if (route.params.names[0] == 2) {
+      global.teamB = [...tasks, newTask]
+    }
     setTask("")
     setAge("")
+
+
   }
 
 
@@ -439,7 +471,6 @@ function PlayerScreen({ navigation, route }) {
                 <Text style={styles.text}>   {editMode ? "Modificar" : "Agregar"}</Text>
               </TouchableOpacity>
             </Pressable>
-
             <Text style={styles.textYearsPlayers}>Numero de años:  {total}</Text>
             <Text style={styles.textYearsPlayers}>Numero de jugadores:  {resultadoPlayer}</Text>
           </View>
